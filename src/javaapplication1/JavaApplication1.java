3042b8aa9e7802de977bd371ff1bdc4b4d7813b9
20401 <20401@1K129S1.dune.edu>
2023-03-06 16:54:02 +0200

    Ошибочное изменение

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -9,6 +9,8 @@ public class JavaApplication1 {
     System.out.println("Hello world!");  
     System.out.println("First change");
     System.out.println("Second change");
+    
+     System.out.println("Message to NewBranch");
     }
     
 }
