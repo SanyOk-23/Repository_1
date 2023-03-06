1ae077b16a4397ad32a2b740760ea1ab4d82e5b7
20401 <20401@1K129S1.dune.edu>
2023-03-06 16:39:51 +0200

    Внесли первое изменение

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -7,7 +7,7 @@ public class JavaApplication1 {
     
     public static void main(String[] args) {
     System.out.println("Hello world!");  
-    
+    System.out.println("First change");
     }
     
 }
